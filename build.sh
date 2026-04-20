dda107977ec8d2df36260cb6f5cf8f2104bc56a7
Ian Chu <chu2815@hotmail.com>
2026-04-20 22:38:53 +0800

    feat: update bundle and loading background

--- build.sh
+++ build.sh
@@ -36,17 +36,7 @@ cp popup.css       dist/
 cp popup.js        dist/
 
 # JS modules
-cp js/app.js       dist/js/
-cp js/auth.js      dist/js/
-cp js/bookmarks.js dist/js/
-cp js/backgroundImage.js dist/js/
-cp js/bgCache.js   dist/js/
-cp js/dragdrop.js  dist/js/
-cp js/driveSync.js dist/js/
-cp js/i18n.js      dist/js/
-cp js/logger.js    dist/js/
-cp js/render.js    dist/js/
-cp js/storage.js   dist/js/
+cp js/*       dist/js/
 
 # Icons (only required sizes)
 cp icons/icon16.png  dist/icons/
